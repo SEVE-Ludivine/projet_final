e595ca26b53cd61ff5711b22d5ffe9a0c7514dc8
ludivine <ludivine.seve@epsi.fr>
2021-02-18 13:58:11 +0100

    Création et nettoyage du projet

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Liste from '../views/Liste.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Liste',
+    component: Liste
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/carte',
+    name: 'Carte',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Carte.vue')
   }
 ]
 
---
+++ src/views/Liste.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="liste">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span v-for="records in dataAPI">
+            <p>{{records.datasetid}}</p>
+            <p>Vous êtes {{records.fields.commune}} {{records.rank}} {{records.leaguePoints}} LP</p>
+            <p>Vous avez fait {{records.wins}} TOP 1.</p>
+        </span>
+  </div>
+</template>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+const apiURL = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parcs-jardins-nantes&q=&facet=libtype&facet=gardien&facet=jeux_enfants&facet=pataugeoire&facet=sanitaires&facet=sanitaires_handicapes&facet=chiens_autorises&facet=jardin_clos&facet=abris&facet=point_eau_potable&facet=table_pique_nique'
+     /*  
+	const demo = new Vue({
+		el: '#demo',
+		data: {
+            dataAPI: []
+        },
+		methods: {
+            fetchData2: async function (pseudo) {
+                const dataAPI= await axios.get(apiURL)
+                this.dataAPI = dataAPI.data
+                console.log(this.dataAPI)
+                console.log(this.dataAPI.records)
+                console.log(this.dataAPI.records.datasetid)
+            }
+
+	  	}
+    })*/
+export default {
+  name: 'Liste des données',
+  components: {
+    HelloWorld
+  }
+}
+</script>
